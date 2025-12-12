2efb67461a2efbaf8303c46fdf174456ec315d5a
KarinaR-Ch <150581037+KarinaR-Ch@users.noreply.github.com>
2025-12-12 11:53:56 +0200

    Delete src directory

--- src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const LOGO =
-  'https://raw.githubusercontent.com/KarinaR-Ch/courses/refs/heads/main/Vector%20(1).png'
--- src/models.ts
+++
@@ -1,8 +0,0 @@
-export type Course = {
-  id: string
-  title: string
-  description: string
-  duration: string
-  created: string
-  authors: string
-}
